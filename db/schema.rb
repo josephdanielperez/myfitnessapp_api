6cea208d92125e044e3e633a51c59e253a800647
Joseph Perez <hello@josephdanielperez.com>
2021-02-03 14:51:13 -0800

    password digest

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_210315) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
