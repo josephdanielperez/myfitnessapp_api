636bf64cea6bf0299ee8fe72ba6957706c374ffe
Joseph Perez <hello@josephdanielperez.com>
2021-02-02 21:47:11 -0800

    finished database

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ e16 = Exercise.create(name: "Chin-Up")
 e17 = Exercise.create(name: "Lat Pull-Down")
 e18 = Exercise.create(name: "Bent Over Barbell Row")
 e19 = Exercise.create(name: "Bent Over Dumbbell Row")
-e20 = Exercise.create(name: "Seated Overhead Press")
+e20 = Exercise.create(name: "Renegade Row")
 e21 = Exercise.create(name: "T-Bar Row")
 e22 = Exercise.create(name: "Seated Cable Row")
 e23 = Exercise.create(name: "Chest Supported Barbell Row")
@@ -71,47 +71,48 @@ e57 = Exercise.create(name: "Dumbbell Romanian Deadlift")
 e58 = Exercise.create(name: "Barbell Straight Leg Deadlift")
 e59 = Exercise.create(name: "Dumbbell Straight Leg Deadlift")
 e60 = Exercise.create(name: "Barbell Sumo Deadlift")
-e61 = Exercise.create(name: "Dumbbell Deadlift")
-e62 = Exercise.create(name: "Glute-Ham Raise")
-e63 = Exercise.create(name: "Cable Pull-Through")
-e64 = Exercise.create(name: "Leg Curl Machine")
-e65 = Exercise.create(name: "Calf Raise")
+e61 = Exercise.create(name: 'Conventional Deadlift')
+e62 = Exercise.create(name: "Dumbbell Deadlift")
+e63 = Exercise.create(name: "Glute-Ham Raise")
+e64 = Exercise.create(name: "Cable Pull-Through")
+e65 = Exercise.create(name: "Leg Curl Machine")
+e66 = Exercise.create(name: "Calf Raise")
 
-e66 = Exercise.create(name: "Standing Barbell Curl")
-e67 = Exercise.create(name: "Standing Dumbbell Curl")
-e68 = Exercise.create(name: "Barbell Preacher Curl")
-e69 = Exercise.create(name: "Dumbbell Preacher Curl")
-e70 = Exercise.create(name: "Seated Dumbbell Curl")
-e71 = Exercise.create(name: "Incline Dumbbell Curl")
-e72 = Exercise.create(name: "Hammer Curl")
-e73 = Exercise.create(name: "Concentration Curl")
-e74 = Exercise.create(name: "Cable Curl")
-e75 = Exercise.create(name: "Bicep Curl Machine")
-e76 = Exercise.create(name: "Close Grip Bench Press")
-e77 = Exercise.create(name: "Decline Close Grip Bench Press")
-e78 = Exercise.create(name: "Close Grip Push-Up")
-e79 = Exercise.create(name: "Laying Barbell Tricep Extension")
-e80 = Exercise.create(name: "Laying Dumbbell Tricep Extension")
-e81 = Exercise.create(name: "Skull Crusher")
-e82 = Exercise.create(name: "Overhead Barbell Tricep Extension")
-e83 = Exercise.create(name: "Overhead Dumbbell Tricep Extension")
-e84 = Exercise.create(name: "Cable Press-Down")
-e85 = Exercise.create(name: "Bench Dip")
+e67 = Exercise.create(name: "Standing Barbell Curl")
+e68 = Exercise.create(name: "Standing Dumbbell Curl")
+e69 = Exercise.create(name: "Barbell Preacher Curl")
+e70 = Exercise.create(name: "Dumbbell Preacher Curl")
+e71 = Exercise.create(name: "Seated Dumbbell Curl")
+e72 = Exercise.create(name: "Incline Dumbbell Curl")
+e73 = Exercise.create(name: "Hammer Curl")
+e74 = Exercise.create(name: "Concentration Curl")
+e75 = Exercise.create(name: "Cable Curl")
+e76 = Exercise.create(name: "Bicep Curl Machine")
+e77 = Exercise.create(name: "Close Grip Bench Press")
+e78 = Exercise.create(name: "Decline Close Grip Bench Press")
+e79 = Exercise.create(name: "Close Grip Push-Up")
+e80 = Exercise.create(name: "Laying Barbell Tricep Extension")
+e81 = Exercise.create(name: "Laying Dumbbell Tricep Extension")
+e82 = Exercise.create(name: "Skull Crusher")
+e83 = Exercise.create(name: "Overhead Barbell Tricep Extension")
+e84 = Exercise.create(name: "Overhead Dumbbell Tricep Extension")
+e85 = Exercise.create(name: "Cable Press-Down")
+e86 = Exercise.create(name: "Bench Dip")
 
-e86 = Exercise.create(name: "Hand Release Push-Up")
-e87 = Exercise.create(name: "Plyo Push-Up")
-e88 = Exercise.create(name: "Russian Twist")
-e89 = Exercise.create(name: "Burpee")
-e90 = Exercise.create(name: "Superman")
-e91 = Exercise.create(name: "Lateral Lunges (Hop Optional)")
-e92 = Exercise.create(name: "Tuck-Up")
-e93 = Exercise.create(name: "Mountain Climber")
-e94 = Exercise.create(name: "Plank")
-e95 = Exercise.create(name: "Plank Jack")
-e96 = Exercise.create(name: "Up-Down Plank")
-e97 = Exercise.create(name: "Squat Thrust")
-e98 = Exercise.create(name: "Flutter Kick")
-e99 = Exercise.create(name: "Jumping Jacks")
+e87 = Exercise.create(name: "Hand Release Push-Up")
+e88 = Exercise.create(name: "Plyo Push-Up")
+e89 = Exercise.create(name: "Russian Twist")
+e80 = Exercise.create(name: "Burpee")
+e91 = Exercise.create(name: "Superman")
+e92 = Exercise.create(name: "Lateral Lunges (Hop Optional)")
+e93 = Exercise.create(name: "Tuck-Up")
+e94 = Exercise.create(name: "Mountain Climber")
+e95 = Exercise.create(name: "Plank")
+e96 = Exercise.create(name: "Plank Jack")
+e97 = Exercise.create(name: "Up-Down Plank")
+e98 = Exercise.create(name: "Squat Thrust")
+e99 = Exercise.create(name: "Flutter Kick")
+e100 = Exercise.create(name: "Jumping Jacks")
 
 s1 = Split.create(name: "Chest")
 s2 = Split.create(name: "Back")
@@ -124,6 +125,7 @@ s8 = Split.create(name: "Upper")
 s9 = Split.create(name: "Lower")
 s10 = Split.create(name: "HITT")
 
+## Chest Exercises ##
 s1.exercises << e1
 s1.exercises << e2
 s1.exercises << e3
@@ -139,6 +141,7 @@ s1.exercises << e12
 s1.exercises << e13
 s1.exercises << e14
 
+## Back Exercises ##
 s2.exercises << e15
 s2.exercises << e16
 s2.exercises << e17
@@ -153,7 +156,13 @@ s2.exercises << e25
 s2.exercises << e26
 s2.exercises << e27
 s2.exercises << e28
+## Back Exercises - Also Leg Exercises ##
+s2.exercises << e56
+s2.exercises << e57
+s2.exercises << e58
+s2.exercises << e59
 
+## Shoulder Exercises ##
 s3.exercises << e29 
 s3.exercises << e30
 s3.exercises << e31
@@ -171,7 +180,8 @@ s3.exercises << e42
 s3.exercises << e43
 s3.exercises << e44
 
-s3.exercises << e45
+## Leg Exercises ##
+s4.exercises << e45
 s4.exercises << e46
 s4.exercises << e47
 s4.exercises << e48
@@ -192,8 +202,9 @@ s4.exercises << e62
 s4.exercises << e63
 s4.exercises << e64
 s4.exercises << e65
+s4.exercises << e66
 
-s5.exercises << e66
+## Arm Exercises ##
 s5.exercises << e67
 s5.exercises << e68
 s5.exercises << e69
@@ -213,10 +224,166 @@ s5.exercises << e82
 s5.exercises << e83
 s5.exercises << e84
 s5.exercises << e85
+s5.exercises << e86
 
-### ADD UPPER, LOWER, PUSH, PULL ###
+## Push Exercises ##
+s6.exercises << e1
+s6.exercises << e2
+s6.exercises << e3
+s6.exercises << e4
+s6.exercises << e5
+s6.exercises << e6
+s6.exercises << e7
+s6.exercises << e8
+s6.exercises << e9
+s6.exercises << e10
+s6.exercises << e11
+s6.exercises << e12
+s6.exercises << e13
+s6.exercises << e14
+s6.exercises << e29 
+s6.exercises << e30
+s6.exercises << e31
+s6.exercises << e32
+s6.exercises << e33
+s6.exercises << e34
+s6.exercises << e38
+s6.exercises << e39
+s6.exercises << e40
+s6.exercises << e41
+s6.exercises << e77
+s6.exercises << e78
+s6.exercises << e79
+s6.exercises << e80
+s6.exercises << e81
+s6.exercises << e82
+s6.exercises << e83
+s6.exercises << e84
+s6.exercises << e85
+s6.exercises << e86
 
-s10.exercises << e86
+## Pull Exercises ##
+s7.exercises << e15
+s7.exercises << e16
+s7.exercises << e17
+s7.exercises << e18
+s7.exercises << e19
+s7.exercises << e21
+s7.exercises << e22
+s7.exercises << e23
+s7.exercises << e24
+s7.exercises << e25
+s7.exercises << e26
+s7.exercises << e27
+s7.exercises << e28
+s7.exercises << e35
+s7.exercises << e36
+s7.exercises << e37
+s7.exercises << e42
+s7.exercises << e43
+s7.exercises << e44
+s7.exercises << e67
+s7.exercises << e68
+s7.exercises << e69
+s7.exercises << e70
+s7.exercises << e71
+s7.exercises << e72
+s7.exercises << e73
+s7.exercises << e74
+s7.exercises << e75
+s7.exercises << e76
+
+## Upper Exercises ##
+s8.exercises << e1
+s8.exercises << e2
+s8.exercises << e3
+s8.exercises << e4
+s8.exercises << e5
+s8.exercises << e6
+s8.exercises << e7
+s8.exercises << e8
+s8.exercises << e9
+s8.exercises << e10
+s8.exercises << e11
+s8.exercises << e12
+s8.exercises << e13
+s8.exercises << e14
+s8.exercises << e15
+s8.exercises << e16
+s8.exercises << e17
+s8.exercises << e18
+s8.exercises << e19
+s8.exercises << e20
+s8.exercises << e21
+s8.exercises << e22
+s8.exercises << e23
+s8.exercises << e24
+s8.exercises << e25
+s8.exercises << e26
+s8.exercises << e27
+s8.exercises << e28
+s8.exercises << e29 
+s8.exercises << e30
+s8.exercises << e31
+s8.exercises << e32
+s8.exercises << e33
+s8.exercises << e34
+s8.exercises << e35
+s8.exercises << e36
+s8.exercises << e37
+s8.exercises << e38
+s8.exercises << e39
+s8.exercises << e40
+s8.exercises << e41
+s8.exercises << e42
+s8.exercises << e43
+s8.exercises << e44
+s8.exercises << e67
+s8.exercises << e68
+s8.exercises << e69
+s8.exercises << e70
+s8.exercises << e71
+s8.exercises << e72
+s8.exercises << e73
+s8.exercises << e74
+s8.exercises << e75
+s8.exercises << e76
+s8.exercises << e77
+s8.exercises << e78
+s8.exercises << e79
+s8.exercises << e80
+s8.exercises << e81
+s8.exercises << e82
+s8.exercises << e83
+s8.exercises << e84
+s8.exercises << e85
+s8.exercises << e86
+
+## Lower Exercises ##
+s9.exercises << e45
+s9.exercises << e46
+s9.exercises << e47
+s9.exercises << e48
+s9.exercises << e49
+s9.exercises << e50
+s9.exercises << e51
+s9.exercises << e52
+s9.exercises << e53
+s9.exercises << e54
+s9.exercises << e55
+s9.exercises << e56
+s9.exercises << e57
+s9.exercises << e58
+s9.exercises << e59
+s9.exercises << e60
+s9.exercises << e61
+s9.exercises << e62
+s9.exercises << e63
+s9.exercises << e64
+s9.exercises << e65
+s9.exercises << e66
+
+## HITT Exercises ##
 s10.exercises << e87
 s10.exercises << e88
 s10.exercises << e89
@@ -230,3 +397,4 @@ s10.exercises << e96
 s10.exercises << e97
 s10.exercises << e98
 s10.exercises << e99
+s10.exercises << e100
