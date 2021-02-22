e795f27bb6c779b3c56828d293756f3938403992
Joseph Perez <hello@josephdanielperez.com>
2021-02-22 08:48:43 -0800

    added video demos to exercises

--- db/migrate/20210202194951_create_exercises.rb
+++ db/migrate/20210202194951_create_exercises.rb
@@ -2,6 +2,7 @@ class CreateExercises < ActiveRecord::Migration[6.0]
   def change
     create_table :exercises do |t|
       t.string :name
+      t.string :url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_14_015526) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
+    t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
