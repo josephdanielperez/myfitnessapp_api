18add2da7b7eb9cfedc9dbc81c4b648737249ac3
Joseph Perez <hello@josephdanielperez.com>
2021-02-28 14:05:40 -0800

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources: :sessions, only: [:create]
-  resources: :registrations, only: [:create]
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
   root to: 'static#home'
 
   resources :exercises
