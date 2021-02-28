ec990251b925862386fe4732d66c3a9889dbec7e
Joseph Perez <hello@josephdanielperez.com>
2021-02-27 20:00:01 -0800

    backend additions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,49 +3,40 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    users = User.all
+    render json: UserSerializer.new(users), status: 200
   end
+
 
   # GET /users/1
   def show
-    render json: @user
+    user = User.find(params[:id])
+    if user 
+      render json: UserSerializer.new(user)
+    else 
+      render json: { error: ['user not found'] }, status: 500
+    end
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    user = User.new(user_params)
+    if user.save
+      login(user)
+      render json: UserSerializer.new(user), status: 200
+    else 
+      render json: {
+        status: 500,
+        error: ['Error creating user!']
+      }
     end
-  end
-
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
       params.require(:user).permit(:name, :username, :password)
     end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :users
   resources :exercises
   resources :splits
+
+  # routes for session controller actions
+  post '/login' => 'sessions#login'
+  post '/signup' => 'users#create'
+  delete '/logout' => 'sessions#logout'
+  get '/current-user' => 'sessions#getUser'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
