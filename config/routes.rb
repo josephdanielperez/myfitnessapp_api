6d4ea62fea71bd88b59f314b3c2d1c922a57e496
Joseph Perez <hello@josephdanielperez.com>
2021-02-28 12:59:23 -0800

    re-doing authentication feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static#home'
+
   resources :exercises
   resources :splits
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_002943) do
+ActiveRecord::Schema.define(version: 2021_02_28_201647) do
 
   create_table "exercise_splits", force: :cascade do |t|
     t.integer "exercise_id"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_02_27_002943) do
 
   create_table "splits", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
