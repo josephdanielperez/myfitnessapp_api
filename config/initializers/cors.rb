9f41178cac1d8178371acb64668b219d260037f5
Joseph Perez <hello@josephdanielperez.com>
2021-03-01 09:03:01 -0800

    allow cookies to be stored

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,6 +4,7 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
+Rails.application.config.action_dispatch.cookies_same_site_protection = :none
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
